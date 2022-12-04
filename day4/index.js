3fa6a106d42a0818a2ee8d872568c3c277db4dc2
Brian Chiruka <brian.chiruka@valr.com>
2022-12-04 17:35:42 +0200

    folder structure

---
+++ day4/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const data = fs.readFileSync("./day4/input.txt", { encoding: "utf8", flag: "r" });
+const array = data.split("\n");
+let count = 0;
+
+const cut = (index, sets) => sets[index].split("-").map(Number);
+const stretch = (array) =>
+  Array.from(
+    new Array(array[1] - array[0] + 1).fill(array[0]),
+    (x, i) => x + i
+  );
+const max = array.length;
+
+for (let i = 0; i < max; i++) {
+  let sets = array[i].split(",");
+  let one = cut(0, sets);
+  let two = cut(1, sets);
+  let stretched1 = stretch(one);
+  let stretched2 = stretch(two);
+  // console.log(array[i])
+  if (
+    stretched1.includes(two[0]) ||
+    stretched1.includes(two[1]) ||
+    stretched2.includes(one[0]) ||
+    stretched2.includes(one[1])
+  ) {
+    count++;
+    // } else {
+    //     console.log(array[i])
+    // }
+  }
+}
+
+console.log(count);
